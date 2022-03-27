ebdfebfbe667e63d894b0eaca025756bd7fde69c
bion <bionjohnson@gmail.com>
2022-03-26 23:20:24 -0700

    workspace file is annoying

--- .doom.d/custom.el
+++ .doom.d/custom.el
@@ -5,7 +5,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(sclang-indent-level 2)
- '(sclang-show-workspace-on-startup false)
+ '(sclang-show-workspace-on-startup nil)
  '(sh-basic-offset 2))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
